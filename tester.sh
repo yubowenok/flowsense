93e333f530c8992bfa98761885ab19e82c7e10e3
Bowen Yu <yubowenok@gmail.com>
2016-12-16 03:08:56 -0500

    + tester

---
+++ tester.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Run under the sempre directory.
+# Assume smartflow is a sibling directory of sempre.
+
+java -cp libsempre/*:lib/* -ea edu.stanford.nlp.sempre.Main -Grammar.inPaths /Users/bowen/smartflow/basic.grammar -Dataset.inPaths test:../smartflow/test/examples.test > test_result
+verdict=`cat test_result | grep "Stats for .*: correct=1"`
+if [ "$verdict" != "" ]; then
+  echo "Correct!"
+  exit 0
+else
+  echo "Incorrect (see test_result for errors)"
+  exit 1
+fi
+
