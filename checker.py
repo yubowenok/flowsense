6ebebb338bda13bfd1928bfad462450eee5e75ae
Bowen Yu <yubowenok@gmail.com>
2017-03-17 22:53:06 -0400

    refine grammar and speed up testing

--- checker.py
+++ checker.py
@@ -2,41 +2,62 @@
 
 import sys, os, re
 
-if len(sys.argv) < 2:
-  print 'no test_result path specified'
+if len(sys.argv) < 3:
+  print 'usage: checker.py result_file test_file'
   sys.exit(1)
   
 test_result_path = sys.argv[1]
+test_input_path = sys.argv[2]
 
-incorrect = 0
+# Which file each test belongs
+test_files = []
+cur_test = ''
+f = open(test_input_path, 'r')
+lines = f.readlines()
+num_lines = len(lines)
+for i in xrange(num_lines):
+  line = lines[i].strip()
+  file_match = re.match('### FILE \((.*)\)', line)
+  if file_match != None:
+    cur_test = file_match.group(1)
+  test_match = re.match('\(example', line)
+  if test_match != None:
+    test_files.append(cur_test)
 
-for dirname, dirnames, filenames in os.walk(test_result_path):
-  for filename in filenames:
-    filepath = os.path.join(dirname, filename)
-    f = open(filepath, 'r')
-    lines = f.readlines()
-    num_lines = len(lines)
-    for i in xrange(num_lines):
-      line = lines[i].strip()
-      if re.match('iter=3.test: example', line) != None:
-        query = re.match('Example: (.*) {', lines[i + 1].strip()).group(1)
-        j = i + 2
-        while j < num_lines:
-          cur_line = lines[j].strip()
-          result = re.match('Current: correct=(.*) oracle', cur_line)
-          if result != None:
-            correct = float(result.group(1))
-            if correct != 1:
-              incorrect += 1
-              print '[%s/%.2f] %s' % (filename, correct, query)
-            i = j;
-            break;
-          j += 1
+
+incorrect, total = 0, 0
+
+f = open(test_result_path, 'r')
+lines = f.readlines()
+num_lines = len(lines)
+for i in xrange(num_lines):
+  line = lines[i].strip()  
+  if re.match('iter=3.test: example', line) != None:
+    total += 1
+    answer = ''
+    query = re.match('Example: (.*) {', lines[i + 1].strip()).group(1)
+    j = i + 2
+    while j < num_lines:
+      cur_line = lines[j].strip()
+     
+      result = re.match('targetValue: \(string "(.*)"\)', cur_line)
+      if result != None: # Get answer
+        answer = result.group(1)
+      
+      result = re.match('Current: correct=(.*) oracle', cur_line)
+      if result != None: # Get verdict
+        correct = float(result.group(1))
+        if correct != 1:
+          incorrect += 1
+          print '[%s/%.2f] %s\nExpected: %s' % (test_files[total - 1], correct, query, answer)
+        i = j;
+        break;
+      j += 1
         
 if incorrect > 0:
-  print '%d queries incorrect' % incorrect
+  print '%d/%d tests incorrect' % (incorrect, total)
   sys.exit(1)
 else:
-  print 'All Correct!'
+  print 'All %d tests correct!' % total
 
 sys.exit(0)
--- tester.sh
+++ tester.sh
@@ -2,23 +2,26 @@
 # Run under the sempre directory.
 # Assume smartflow is a sibling directory of sempre.
 
-if [ ! -d test_result ]; then
-  mkdir test_result
-else
-  rm test_result/*
-fi
+allresult="../all.result"
+alltest="../all.test"
+
+if [ -f $allresult ]; then rm $allresult; fi
+if [ -f $alltest ]; then rm $alltest; fi
 
 for testfile in ../test/*.test; do
   file=$(basename "$testfile")
-  java -cp libsempre/*:lib/* -ea edu.stanford.nlp.sempre.Main\
-       -languageAnalyzer corenlp.CoreNLPAnalyzer\
-       -Grammar.inPaths ../main.grammar\
-       -FeatureExtractor.featureDomains rule\
-       -Learner.maxTrainIters 3\
-       -Dataset.inPaths\
-         train:../data/train.examples\
-         test:$testfile > test_result/$file
+  echo "### FILE ($file)" >> $alltest
+  cat $testfile >> $alltest
 done
 
-python ../checker.py test_result
+java -cp libsempre/*:lib/* -ea edu.stanford.nlp.sempre.Main\
+    -languageAnalyzer corenlp.CoreNLPAnalyzer\
+    -Grammar.inPaths ../main.grammar\
+    -FeatureExtractor.featureDomains rule\
+    -Learner.maxTrainIters 3\
+    -Dataset.inPaths\
+     train:../data/train.examples\
+     test:$alltest > $allresult
+
+python ../checker.py $allresult $alltest
 exit $?
